ffd7efd82b61be7ef1b12819be86152b2c32787c
kieferjackson <kieferleejackson@gmail.com>
2022-11-01 15:59:35 -0700

    Added more user routes

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,5 @@
 const User = require('../models/User');
+const Thought = require('../models/Thought');
 
 module.exports = 
 {
@@ -23,5 +24,53 @@ module.exports =
         User.create(req.body)
             .then( (new_userdata) => res.json(new_userdata))
             .catch( (error) => res.status(500).json(error));
+    },
+    
+    updateUser(req, res)
+    {
+        User.findOneAndUpdate
+        (
+            { _id: req.params.userId },
+            { $set: req.body },
+            { runValidators: true, new: true }
+        ).then( (user) => !user
+            ? res.status(404).json({ message: `No user with ID: ${req.params.userId}`}) 
+            : res.json(user)
+        ).catch( (error) => res.status(500).json(error));
+    },
+
+    deleteUser(req, res)
+    {
+        User.findOneAndRemove({ _id: req.params.userId })
+        .then( (user) => !user
+            ? res.status(404).json({ message: `No user with ID: ${req.params.userId}`}) 
+            : res.status(200).json({ message: `User was successfully deleted` })
+        ).catch( (error) => res.status(500).json(error));
+    },
+
+    addFriend(req, res)
+    {
+        User.findOneAndUpdate
+        (
+            { _id: req.params.userId },
+            { $addToSet: { friends: req.body } },
+            { runValidators: true, new: true }
+        ).then( (user) => !user
+            ? res.status(404).json({ message: `No user with ID: ${req.params.userId}`}) 
+            : res.status(200).json(user)
+        ).catch( (error) => res.status(500).json(error));
+    },
+
+    removeFriend(req, res)
+    {
+        User.findOneAndUpdate
+        (
+            { _id: req.params.videoId },
+            { $pull: { friends: { responseId: req.params.friendId } } },
+            { runValidators: true, new: true }
+        ).then( (user) => !user
+            ? res.status(404).json({ message: `No user with ID: ${req.params.userId}`}) 
+            : res.status(200).json(user)
+        ).catch( (error) => res.status(500).json(error));
     }
 }

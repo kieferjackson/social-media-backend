2d818e2acc9e6c44f2252db0280cf49be2ac85d8
kieferjackson <kieferleejackson@gmail.com>
2022-11-14 15:00:57 -0700

    Changed reaction DELETE route

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -15,6 +15,9 @@ router.route('/').get(getThoughts).post(createThought);
 router.route('/:thoughtId').get(getSingleThought).put(updateThought).delete(deleteThought);
 
 // /api/thoughts/:thoughtId/reactions
-router.route('/:thoughtId/reactions').post(addReaction).delete(removeReaction);
+router.route('/:thoughtId/reactions').post(addReaction);
+
+// /api/thoughts/:thoughtId/reactions/:reactionId
+router.route('/:thoughtId/reactions/:reactionId').delete(removeReaction);
 
 module.exports = router;
